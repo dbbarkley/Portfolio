2fe169a4019a12232544ec4911f954d9a63e41b4
Daniel Barkley <danielbarkley@daniels-mbp-2.attlocal.net>
2019-02-21 00:03:08 -0600

    got the portfolio page to an acceptable state. Started working on the Contactpage.

--- assets/JS/app.js
+++ assets/JS/app.js
@@ -11,3 +11,6 @@ $('.button-collapse').sideNav({
     closeOnClick: true, // Closes side-nav on <a> clicks, useful for Angular/Meteor
     draggable: true, // Choose whether you can drag to open on touch screens,
 });
+
+$('#textarea1').val('');
+$('#textarea1').trigger('autoresize');
